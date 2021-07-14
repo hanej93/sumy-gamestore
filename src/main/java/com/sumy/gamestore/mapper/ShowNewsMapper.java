8253feeaa18fa32ee103b09416b8c922427afd37
영선 <hallellall@naver.com>
2021-07-14 16:37:32 +0900

    [추가] 메인페이지 (ShowGameMapper , ShowNewsMapper) 쿼리문 추가

--- src/main/java/com/sumy/gamestore/mapper/ShowNewsMapper.java
+++ src/main/java/com/sumy/gamestore/mapper/ShowNewsMapper.java
@@ -1,5 +1,15 @@
 package com.sumy.gamestore.mapper;
 
-public interface ShowNewsMapper {
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import com.sumy.gamestore.model.NewsList;
+@Mapper
+public interface ShowNewsMapper {
+	
+	@Select("select news_id, news_title, news_sub_title, "
+			+ "news_thumb_image, news_write_date, news_update_date "
+			+ "from news_list order by news_id desc limit 3")
+	public NewsList selectNewsByNewsId();
 }
+
