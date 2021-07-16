5795049db5ff3b18e3f7811404d54e47bb9de185
명주 <javacake93@gmail.com>
2021-07-16 12:22:45 +0900

    [추가]회원정보 변경 컨트롤러 추가

--- src/main/java/com/sumy/gamestore/controller/MyPageController.java
+++ src/main/java/com/sumy/gamestore/controller/MyPageController.java
@@ -1,19 +1,34 @@
 package com.sumy.gamestore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sumy.gamestore.service.UpdateUserService;
 
 
 @Controller
 @RequestMapping("/sumy")
 public class MyPageController {
+	
+	@Autowired
+	UpdateUserService updateUserService;
 
 	// 회원 정보 수정 화면
 	@GetMapping("/login-security")
 	public String test7() {
 		System.out.println("회원정보수정");
 		return "user/page-login-security-1";
+	}
+	
+	// 프로필 사진 업데이트
+	@PostMapping("/profileImgUpdate")
+	public String profileImgUpdate(@RequestPart(value = "file", required = false) MultipartFile file) {
+		System.out.println("프로필이미지 변경중");
 	}
 
 	// 주문 내역 화면
