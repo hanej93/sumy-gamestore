a4ae11e31800e3487f69696e8c7f4c3bc0faae3c
명주 <javacake93@gmail.com>
2021-07-16 11:59:55 +0900

    [변경]AddUserService -> UpdateUserService로 변경

--- src/main/java/com/sumy/gamestore/service/UpdateUserService.java
+++ src/main/java/com/sumy/gamestore/service/UpdateUserService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserAddService {
+public class UpdateUserService {
 //	@Autowired
 	
 
